f691bd0c7cbcdb70cf001d011832b83fa1995311
Eyal Cohen <eyalc82@gmail.com>
2013-04-28 01:41:33 +0300

    Added ListStyleImage to support custom image itmes

---
+++ ECListView/ECListView.m
@@ -0,0 +1,31 @@
+//
+//  ECListView.m
+//  ECListView
+//
+//  Created by Eyal Cohen on 4/27/13.
+//  Copyright (c) 2013 Eyal Cohen. All rights reserved.
+//
+
+#import "ECListView.h"
+
+@implementation ECListView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
