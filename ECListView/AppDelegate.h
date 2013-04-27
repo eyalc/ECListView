cf21ce571ac6d6613383f4b6a0b7e02f62bfd9de
Eyal Cohen <eyalc82@gmail.com>
2013-04-27 21:32:49 +0300

    Initial commit

---
+++ ECListView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ECListView
+//
+//  Created by Eyal Cohen on 4/27/13.
+//  Copyright (c) 2013 Eyal Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ECListView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ECListView
+//
+//  Created by Eyal Cohen on 4/27/13.
+//  Copyright (c) 2013 Eyal Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
